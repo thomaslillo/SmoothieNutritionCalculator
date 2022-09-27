de680587d6586c6903e90bd604e7dd2788ec2fdb
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-09-27 12:31:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,13 @@ display_selected_fruits = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(display_selected_fruits)
 
-st.metric(label="Total Calories", value="70 °F", delta="1.2 °F")
-st.metric(label="Total Fat (G)", value="70 °F", delta="1.2 °F")
-st.metric(label="Total Protine (G)", value="70 °F", delta="1.2 °F")
+
+# display the health information
+col1, col2, col3 = st.columns(3)
+
+col1.metric(label="Total Calories", value="70 °F", delta="1.2 °F")
+col2.metric(label="Total Fat (G)", value="70 °F", delta="1.2 °F")
+col3.metric(label="Total Protine (G)", value="70 °F", delta="1.2 °F")
 
 # all fruits
 st.header('All Fruits')
