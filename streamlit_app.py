b0fd473f738de2c56fdb512a09f8c72d626bd48f
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-09-27 13:10:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ st.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
