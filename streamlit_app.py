055df0332577f1a5391406f50171d0a27d69826e
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-09-27 12:30:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
-import streamlit
+import streamlit as st
 import pandas
 
-streamlit.header('Smoothie Nutrition')
-streamlit.text('Figure out how healthy your smoothies are.')
+st.header('Smoothie Nutrition')
+st.text('Figure out how healthy your smoothies are.')
 
-streamlit.header('Build your own smoothie below!')
+st.header('Build your own smoothie below!')
 
 # bring in the data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -12,14 +12,18 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # list the fruits
-fruits_selected = streamlit.multiselect("Pick Fruits:", list(my_fruit_list.index))
+fruits_selected = st.multiselect("Pick Fruits:", list(my_fruit_list.index))
 
-streamlit.header('Your Selected Fruits')
+st.header('Your Selected Fruits')
 display_selected_fruits = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(display_selected_fruits)
+st.dataframe(display_selected_fruits)
+
+st.metric(label="Total Calories", value="70 °F", delta="1.2 °F")
+st.metric(label="Total Fat (G)", value="70 °F", delta="1.2 °F")
+st.metric(label="Total Protine (G)", value="70 °F", delta="1.2 °F")
 
 # all fruits
-streamlit.header('All Fruits')
+st.header('All Fruits')
 
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
