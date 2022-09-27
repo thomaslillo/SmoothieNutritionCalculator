e615101e0feafeb3f224edbca8f2f1aa424dd7f7
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-09-27 15:28:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,20 +60,25 @@ def App():
   # get the smoothie stats
   st.header("Your Smoothie's Stats")
   
-  total_cals = smoothie_nutri_total(all_fruits, 'Calories', fruit_counts)
-  # total_carbs = smoothie_nutri_total(all_fruits, 'Carbohydrates', fruit_counts)
-  # total_protine = smoothie_nutri_total(all_fruits, 'Protein', fruit_counts)
-  # total_fat = smoothie_nutri_total(all_fruits, 'Fat', fruit_counts)
-  # total_sugar = smoothie_nutri_total(all_fruits, 'Sugar', fruit_counts)      
+  
+  total_carbs = smoothie_nutri_total(all_fruits, 'Carbohydrates', fruit_counts)
+  total_protine = smoothie_nutri_total(all_fruits, 'Protein', fruit_counts)
+  total_fat = smoothie_nutri_total(all_fruits, 'Fat', fruit_counts)
+  total_sugar = smoothie_nutri_total(all_fruits, 'Sugar', fruit_counts)      
   
   # display the stats
   cals, carbs, protine, fat, sugar = st.columns(5)
   
-  # cals.metric(label="Calories of the Smoothie (per 100g) in Grams", value=str(total_cals), delta="1.2 °F")  
-  # carbs.metric(label="Carbohydrates of the Smoothie (per 100g) in Grams", value=str(total_carbs), delta="1.2 °F")
-  # protine.metric(label="Protein of the Smoothie (per 100g) in Grams", value=str(total_protine), delta="1.2 °F")
-  # fat.metric(label="Fat of the Smoothie (per 100g) in Grams", value=str(total_fat), delta="1.2 °F")    
-  # sugar.metric(label="Sugar of the Smoothie (per 100g) in Grams", value=str(total_sugar), delta="1.2 °F")      
+  total_cals = str(smoothie_nutri_total(all_fruits, 'Calories', fruit_counts))
+  cals.metric(label="Calories of the Smoothie (per 100g) in Grams", value=total_cals, delta="1.2 °F")  
+  
+  carbs.metric(label="Carbohydrates of the Smoothie (per 100g) in Grams", value=str(total_carbs), delta="1.2 °F")
+  
+  protine.metric(label="Protein of the Smoothie (per 100g) in Grams", value=str(total_protine), delta="1.2 °F")
+  
+  fat.metric(label="Fat of the Smoothie (per 100g) in Grams", value=str(total_fat), delta="1.2 °F")    
+  
+  sugar.metric(label="Sugar of the Smoothie (per 100g) in Grams", value=str(total_sugar), delta="1.2 °F")      
     
   # the all fruits reference table  
   st.header('All Fruits Reference')
