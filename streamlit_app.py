6de76051ec7985842d7499b03f949dee30d830ee
Thomas Lillo <thomas.j.lillo@gmail.com>
2022-09-27 12:17:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,10 @@ streamlit.text('Figure out how healthy your smoothies are.')
 
 streamlit.header('Build your own smoothie below!')
 
+# bring in the data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # list the fruits
 streamlit.multiselect("Pick Fruits:", list(my_fruit_list.Fruit))
